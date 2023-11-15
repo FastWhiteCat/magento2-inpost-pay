e75c7d598a7f36099e375254282c6604239a9c73
Patryk Grabiec <patryk.grabiec@fastwhitecat.com>
2023-11-15 17:44:22 +0100

    INPAY-28 test commit

---
+++ packages/magento2-module-inpost-pay/src/Model/IziApi/Request/BindingBasketRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InPost\InPostPay\Model\IziApi\Request;
+
+use InPost\InPostPay\Api\ApiConnector\RequestInterface;
+use InPost\InPostPay\Provider\Config\IziApiConfigProvider;
+use InPost\InPostPay\Service\ApiConnector\TokenGenerator;
+use Laminas\Http\Request;
+
+class BindingBasketRequest implements RequestInterface
+{
+    public const VERSION = 'version';
+    public const URI_PATTERN = '/v1/izi/basket/{basket_id}/binding';
+
+    private string $method = Request::METHOD_POST;
+    private ?string $contentType = null;
+    private array $params = [];
+
+    /**
+     * @param IziApiConfigProvider $iziApiConfigProvider
+     * @param TokenGenerator $tokenGenerator
+     */
+    public function __construct(
+        private readonly IziApiConfigProvider $iziApiConfigProvider,
+        private readonly TokenGenerator $tokenGenerator
+    ) {
+    }
+
+    public function getUri(): string
+    {
+        $uri = self::URI_PATTERN;
+        foreach ($this->getParams() as $key => $value) {
+            $uri = str_replace(sprintf('{%s}', (string)$key), (string)$value, $uri);
+        }
+
+        return (string)preg_replace('/{[a-zA-Z0-9_-]*}/', '', $uri);
+    }
+
+    public function getApiUrl(): string
+    {
+        return $this->iziApiConfigProvider->getIziApiUrl();
+    }
+
+    public function setParams(array $params): void
+    {
+        $this->params = $params;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function getContentType(): ?string
+    {
+        return $this->contentType;
+    }
+
+    public function getBearerToken(): ?string
+    {
+        return $this->tokenGenerator->generate()->getAccessToken();
+    }
+}
