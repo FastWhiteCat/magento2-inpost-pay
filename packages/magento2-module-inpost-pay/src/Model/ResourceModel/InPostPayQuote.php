eb5af87f63f0d9d0ca8dc507be85a893167cb7bb
Patryk Grabiec <patryk.grabiec@fastwhitecat.com>
2023-11-17 12:09:02 +0100

    INPAY-28 added new table inpost_pay_quote

---
+++ packages/magento2-module-inpost-pay/src/Model/ResourceModel/InPostPayQuote.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InPost\InPostPay\Model\ResourceModel;
+
+use InPost\InPostPay\Api\Data\InPostPayQuoteInterface;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class InPostPayQuote extends AbstractDb
+{
+    protected function _construct(): void
+    {
+        $this->_init(InPostPayQuoteInterface::ENTITY_NAME, InPostPayQuoteInterface::ENTITY_ID);
+    }
+}
