59cbe6004f36b5d45a4349be148627c8c54e1e34
Patryk Grabiec <patryk.grabiec@fastwhitecat.com>
2023-11-21 10:27:05 +0100

    INPAY-28 fixed problem with post in connector

--- packages/magento2-module-inpost-pay/src/Service/ApiConnector/Connector.php
+++ packages/magento2-module-inpost-pay/src/Service/ApiConnector/Connector.php
@@ -35,7 +35,11 @@ class Connector implements ConnectorInterface
 
         try {
             $this->createRequestLog($url, $headers, $params);
-            $response = $client->{$request->getMethod()}($url, !empty($params) ? ['form_params' => $params] : []);
+            if (count($params) === count($params, COUNT_RECURSIVE)) {
+                $response = $client->{$request->getMethod()}($url, !empty($params) ? ['form_params' => $params] : []);
+            } else {
+                $response = $client->{$request->getMethod()}($url, !empty($params) ? $params : []);
+            }
         } catch (Exception $e) {
             $errorMsg = __('InPost API endpoint "%1" responded with an error: %2', $url, $e->getMessage());
             $this->createResponseLog($errorMsg->render(), $e->getCode(), true);
