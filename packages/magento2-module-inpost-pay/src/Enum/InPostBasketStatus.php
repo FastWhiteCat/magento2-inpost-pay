b147ebd99d075dc965f9edce21cf4dea4eb99bec
Marek Tytuła <marek.tytula@fastwhitecat.com>
2023-12-04 11:54:50 +0100

    INPAY-29 Fixed phpcs

--- packages/magento2-module-inpost-pay/src/Enum/InPostBasketStatus.php
+++ packages/magento2-module-inpost-pay/src/Enum/InPostBasketStatus.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace InPost\InPostPay\Enum;
 
+/**
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.Incorrect
+ * @phpcs:disable Generic.WhiteSpace.ScopeIndent.IncorrectExact
+ */
 enum InPostBasketStatus: string
 {
     case PENDING = 'PENDING';
