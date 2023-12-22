e98c1de3e373097d946402dcfc5eccfd3ec49353
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2023-12-22 15:38:42 +0100

    INPAY-55 - Hide inpay button in minicart if in cart is not allowed product, code refactoring

--- packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
+++ packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
@@ -330,7 +330,7 @@ define([
             });
 
             function checkCartWidget(cartData = "") {
-                var wrapperClass = getConfig().wrapperClass || "inpost-widget-wrapper";
+                var wrapperClass = getConfig().wrapperClass || "inpay-widget-wrapper";
                 var popupBindingPlace = getConfig().popupBindingPlace || "BASKET_POPUP";
                 var $inpayWrapperOnBasket = $("." + wrapperClass + "." + popupBindingPlace);
                 var counter = cartData ? cartData.summary_count : getConfig().count;
