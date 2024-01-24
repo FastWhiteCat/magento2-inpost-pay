605068d3b4afc80abbe8bcf7dee80360094a768d
Patryk Grabiec <patryk.grabiec@fastwhitecat.com>
2024-01-24 16:09:50 +0100

    INPAY-94 fixed problem with missing basketId

--- packages/magento2-module-inpost-pay/src/Service/DataTransfer/QuoteToBasket/QuoteToBasketProductsDataTransfer.php
+++ packages/magento2-module-inpost-pay/src/Service/DataTransfer/QuoteToBasket/QuoteToBasketProductsDataTransfer.php
@@ -16,6 +16,7 @@ use InPost\InPostPay\Service\CreateBasketNotice;
 use InPost\InPostPay\Service\DataTransfer\ProductToInPostProduct\ProductToInPostProductDataTransfer;
 use InPost\Restrictions\Api\Data\RestrictionsRuleInterface;
 use InPost\Restrictions\Provider\RestrictedProductIdsProvider;
+use Magento\Catalog\Model\Product\Type;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Item;
