7cd4e6b008bed73344ca32c9ead8f2e3d90f8289
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2024-02-07 22:01:05 +0100

    INPAY-102 - removed comments

--- packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
+++ packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
@@ -138,22 +138,16 @@ define([
             });
 
             function getCookie(name) {
-                // Split cookie string and get all individual name=value pairs in an array
-                let cookieArr = document.cookie.split(";");
+                var cookieArr = document.cookie.split(";");
 
-                // Loop through the array elements
-                for(let i = 0; i < cookieArr.length; i++) {
+                for (let i = 0; i < cookieArr.length; i++) {
                     let cookiePair = cookieArr[i].split("=");
 
-                    /* Removing whitespace at the beginning of the cookie name
-                    and compare it with the given string */
-                    if(name == cookiePair[0].trim()) {
-                        // Decode the cookie value and return
+                    if (name === cookiePair[0].trim()) {
                         return decodeURIComponent(cookiePair[1]);
                     }
                 }
 
-                // Return null if not found
                 return null;
             }
         },
