ef782e72991a450336003824ad23e980d83682e5
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2024-02-07 21:58:41 +0100

    INPAY-102 - removed from payload data prefix and phone number if carts are not binded

--- packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
+++ packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
@@ -95,9 +95,12 @@ define([
         iziGetPayData: function (prefix, phoneNumber, bindingPlace) {
             var url = urlBuilder.build('inpostizi/PayData/Get' + '/form_key/' + $.mage.cookies.get('form_key'));
             var browserData = window.iziGetBrowserData({base64: true});
+            var browserId = getCookie('BrowserId');
+            var prefixValue = !browserId && prefix ? "" : prefix ? "+" + prefix : "";
+            var phoneNumberValue = !browserId && phoneNumber ? "" : phoneNumber ? phoneNumber : "";
             var data = {
-                prefix: prefix && "+" + prefix || "",
-                number: phoneNumber || "",
+                prefix: prefixValue, //prefix && "+" + prefix || "",
+                number: phoneNumberValue, //phoneNumber || "",
                 browser: browserData,
                 binding_place: bindingPlace
             };
@@ -133,6 +136,26 @@ define([
                         reject(new Error($.mage.__('Network problem')));
                     });
             });
+
+            function getCookie(name) {
+                // Split cookie string and get all individual name=value pairs in an array
+                let cookieArr = document.cookie.split(";");
+
+                // Loop through the array elements
+                for(let i = 0; i < cookieArr.length; i++) {
+                    let cookiePair = cookieArr[i].split("=");
+
+                    /* Removing whitespace at the beginning of the cookie name
+                    and compare it with the given string */
+                    if(name == cookiePair[0].trim()) {
+                        // Decode the cookie value and return
+                        return decodeURIComponent(cookiePair[1]);
+                    }
+                }
+
+                // Return null if not found
+                return null;
+            }
         },
 
         iziGetBrowserData: function (params) {
