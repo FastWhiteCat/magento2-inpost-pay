59c0d92817b16f9dea30bd5756121301494dae5d
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2024-04-02 12:39:29 +0200

    INPAY-149, INPAY-150 - added inpay button on login and register pages, added configuration in PA, added new field in grapqhl schema, change success page binding place name

--- packages/magento2-module-inpost-pay/src/Provider/Config/DisplayConfigProvider.php
+++ packages/magento2-module-inpost-pay/src/Provider/Config/DisplayConfigProvider.php
@@ -13,10 +13,14 @@ class DisplayConfigProvider
     private const XML_PATH_ENABLED_ON_CART = 'payment/inpost_pay/show_on_cart';
     private const XML_PATH_ENABLED_IN_MINICART = 'payment/inpost_pay/show_in_minicart';
     private const XML_PATH_ENABLED_ON_SUCCESS_PAGE = 'payment/inpost_pay/show_on_success_page';
+    private const XML_PATH_ENABLED_ON_REGISTER_PAGE = 'payment/inpost_pay/show_on_register_page';
+    private const XML_PATH_ENABLED_ON_LOGIN_PAGE = 'payment/inpost_pay/show_on_login_page';
     public const PRODUCT_CARD_BINDING_PLACE_NAME = 'PRODUCT_CARD';
     public const BASKET_SUMMARY_BINDING_PLACE_NAME = 'BASKET_SUMMARY';
     public const BASKET_POPUP_BINDING_PLACE_NAME = 'BASKET_POPUP';
-    public const THANK_YOU_PAGE_BINDING_PLACE_NAME = 'THANK_YOU_PAGE';
+    public const THANK_YOU_PAGE_BINDING_PLACE_NAME = 'ORDER_CREATE';
+    public const REGISTER_PAGE_BINDING_PLACE_NAME = 'REGISTERFORM_PAGE';
+    public const LOGIN_PAGE_BINDING_PLACE_NAME = 'LOGIN_PAGE';
 
     /**
      * @param ScopeConfigInterface $scopeConfig
@@ -66,6 +70,28 @@ class DisplayConfigProvider
     {
         return $this->scopeConfig->isSetFlag(
             self::XML_PATH_ENABLED_ON_SUCCESS_PAGE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabledOnRegisterPage(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLED_ON_REGISTER_PAGE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabledOnLoginPage(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLED_ON_LOGIN_PAGE,
             ScopeInterface::SCOPE_STORE
         );
     }
