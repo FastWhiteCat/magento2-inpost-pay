576d4b530b9b9104405e73e989c6a32952543bb6
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2024-04-03 13:00:30 +0200

    INPAY-150 - added config model to inpost configuration on checkout page, added configuration to checkout enabling inpay button, adjusted widget to handle every places

--- packages/magento2-module-inpost-pay/src/Provider/Config/DisplayConfigProvider.php
+++ packages/magento2-module-inpost-pay/src/Provider/Config/DisplayConfigProvider.php
@@ -11,12 +11,14 @@ class DisplayConfigProvider
 {
     private const XML_PATH_ENABLED_ON_PRODUCT_CART = 'payment/inpost_pay/show_on_product_cart';
     private const XML_PATH_ENABLED_ON_CART = 'payment/inpost_pay/show_on_cart';
+    private const XML_PATH_ENABLED_ON_CHECKOUT = 'payment/inpost_pay/show_on_checkout';
     private const XML_PATH_ENABLED_IN_MINICART = 'payment/inpost_pay/show_in_minicart';
     private const XML_PATH_ENABLED_ON_SUCCESS_PAGE = 'payment/inpost_pay/show_on_success_page';
     public const PRODUCT_CARD_BINDING_PLACE_NAME = 'PRODUCT_CARD';
     public const BASKET_SUMMARY_BINDING_PLACE_NAME = 'BASKET_SUMMARY';
     public const BASKET_POPUP_BINDING_PLACE_NAME = 'BASKET_POPUP';
     public const THANK_YOU_PAGE_BINDING_PLACE_NAME = 'THANK_YOU_PAGE';
+    public const CHECKOUT_PAGE_BINDING_PLACE_NAME = 'CHECKOUT';
 
     /**
      * @param ScopeConfigInterface $scopeConfig
@@ -66,6 +68,17 @@ class DisplayConfigProvider
     {
         return $this->scopeConfig->isSetFlag(
             self::XML_PATH_ENABLED_ON_SUCCESS_PAGE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabledOnCheckoutPage(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLED_ON_CHECKOUT,
             ScopeInterface::SCOPE_STORE
         );
     }
