8aa952053c8cf99dfda02e21fcb9f5056ffbd05b
Patryk Grabiec <patryk.grabiec@fastwhitecat.com>
2024-04-22 10:38:49 +0200

    INPAY-154 removed phpDocs

--- packages/magento2-module-inpost-pay/src/Block/Adminhtml/System/Config/SynchronizePaymentMethodsButton.php
+++ packages/magento2-module-inpost-pay/src/Block/Adminhtml/System/Config/SynchronizePaymentMethodsButton.php
@@ -6,17 +6,12 @@ use Magento\Backend\Block\Template\Context;
 use Magento\Backend\Block\Widget\Button;
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Framework\Exception\LocalizedException;
 
 class SynchronizePaymentMethodsButton extends Field
 {
     /** @var UrlInterface */
     protected $_urlBuilder;
 
-    /**
-     * @param Context $context
-     * @param array $data
-     */
     public function __construct(
         Context $context,
         array $data = []
@@ -25,23 +20,12 @@ class SynchronizePaymentMethodsButton extends Field
         parent::__construct($context, $data);
     }
 
-    /**
-     * Set template
-     *
-     * @return void
-     */
     protected function _construct()
     {
         parent::_construct();
         $this->setTemplate('InPost_InPostPay::system/config/synchronizePaymentMethodsButton.phtml');
     }
 
-    /**
-     * Generate button html
-     *
-     * @return string
-     * @throws LocalizedException
-     */
     public function getButtonHtml()
     {
         $button = $this->getLayout()->createBlock(
@@ -64,25 +48,12 @@ class SynchronizePaymentMethodsButton extends Field
         );
     }
 
-    /**
-     * Render button
-     *
-     * @param  AbstractElement $element
-     * @return string
-     */
     public function render(AbstractElement $element)
     {
         $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
         return parent::render($element);
     }
 
-    /**
-     * Return element html
-     *
-     * @param  AbstractElement $element
-     * @return string
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
     protected function _getElementHtml(AbstractElement $element)
     {
         return $this->_toHtml();
