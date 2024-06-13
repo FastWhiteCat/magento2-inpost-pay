b9d77f71655ba3f2301324c1d6d80660033994d2
Patryk Grabiec <patryk.grabiec@fastwhitecat.com>
2024-06-13 14:54:19 +0200

    INPAY-173 fixed problem with placing order with last product in cart

---
+++ packages/magento2-module-inpost-pay/src/Plugin/ConfigurableProduct/CartItemProcessorPlugin.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace InPost\InPostPay\Plugin\ConfigurableProduct;
+
+use InPost\InPostPay\Api\Data\Merchant\Basket\Summary\NoticeInterfaceFactory;
+use Magento\ConfigurableProduct\Model\Quote\Item\CartItemProcessor;
+use Magento\Quote\Api\Data\CartItemInterface;
+
+class CartItemProcessorPlugin
+{
+    public function afterConvertToBuyRequest(CartItemProcessor $subject, $result, CartItemInterface $cartItem)
+    {
+        if ($result
+            && $cartItem->getProductOption()
+            && $cartItem->getProductOption()->getExtensionAttributes()
+        ) {
+            $result->setData('id', $cartItem->getItemId());
+        }
+    }
+}
