05e1d18714c5bc1fc9c1bbf1db1f00269f6d0657
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2024-07-11 17:13:20 +0200

    INPAY-177 - Added OMNIBUS flag to promo codes configuraton and API provider.

---
+++ packages/magento2-module-inpost-pay/src/Model/Config/Source/SalesRules.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InPost\InPostPay\Model\Config\Source;
+
+use Magento\Framework\Data\Collection;
+use Magento\Framework\Data\OptionSourceInterface;
+use Magento\SalesRule\Model\Data\Rule;
+use Magento\SalesRule\Model\Rule as RuleModel;
+use Magento\SalesRule\Model\ResourceModel\Rule\CollectionFactory as SalesRuleCollectionFactory;
+use Magento\SalesRule\Model\ResourceModel\Rule\Collection as SalesRuleCollection;
+
+class SalesRules implements OptionSourceInterface
+{
+    /**
+     * @param SalesRuleCollectionFactory $salesRuleCollectionFactory
+     */
+    public function __construct(
+        private readonly SalesRuleCollectionFactory $salesRuleCollectionFactory,
+    ) {
+    }
+
+    /**
+     * @return array[]
+     */
+    public function toOptionArray(): array
+    {
+        $salesRules = $this->getSalesRules();
+        $salesRulesOptions = [];
+
+        foreach ($salesRules as $salesRule) {
+            $label = (string)$salesRule->getName();
+            $value = (int)$salesRule->getRuleId();
+            $salesRulesOptions[] = [
+                'value' => $value,
+                'label' => $label
+            ];
+        }
+
+        return $salesRulesOptions;
+    }
+
+    /**
+     * @return Rule[]
+     */
+    private function getSalesRules(): array
+    {
+        /** @var SalesRuleCollection $attributeCollection */
+        $salesRuleCollection = $this->salesRuleCollectionFactory->create();
+        $salesRuleCollection->addFieldToFilter(
+            Rule::KEY_COUPON_TYPE,
+            ['neq' => RuleModel::COUPON_TYPE_NO_COUPON]
+        );
+        $salesRuleCollection->addOrder(Rule::KEY_RULE_ID, Collection::SORT_ORDER_ASC);
+        $salesRules = [];
+
+        foreach ($salesRuleCollection->getItems() as $salesRule) {
+            if ($salesRule instanceof RuleModel) {
+                $salesRules[] = $salesRule;
+            }
+        }
+
+        return $salesRules;
+    }
+}
---
+++ packages/magento2-module-inpost-pay/src/Provider/Config/OmnibusConfigProvider.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InPost\InPostPay\Provider\Config;
+
+use InPost\InPostPay\Model\Config\Source\ProductAttributes;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class OmnibusConfigProvider
+{
+    private const XML_PATH_OMNIBUS_SALESRULES = 'payment/inpost_pay/omnibus_salesrules';
+    private const XML_PATH_OMNIBUS_LOWEST_PRICE_ATTR = 'payment/inpost_pay/omnibus_lowest_price_product_attribute';
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(private readonly ScopeConfigInterface $scopeConfig)
+    {
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getOmnibusCartPriceRuleIds(): array
+    {
+        $configValue = $this->scopeConfig->getValue(
+            self::XML_PATH_OMNIBUS_SALESRULES,
+            ScopeInterface::SCOPE_WEBSITE
+        );
+
+        $ruleIdsCombined = explode(',', is_scalar($configValue) ? (string)$configValue : '');
+        $ruleIds = [];
+
+        if ($ruleIdsCombined) {
+            foreach ($ruleIdsCombined as $ruleId) {
+                $ruleIds[] = (int)$ruleId;
+            }
+        }
+
+        return $ruleIds;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOmnibusProductAttributeCode(): ?string
+    {
+        $configValue = $this->scopeConfig->getValue(
+            self::XML_PATH_OMNIBUS_LOWEST_PRICE_ATTR,
+            ScopeInterface::SCOPE_STORE
+        );
+
+        if (empty($configValue)
+            || !is_scalar($configValue)
+            || $configValue === ProductAttributes::NOT_SELECTED_ATTRIBUTE_VALUE
+        ) {
+            $attributeCode = null;
+        } else {
+            $attributeCode = (string)$configValue;
+        }
+
+        return $attributeCode;
+    }
+}
