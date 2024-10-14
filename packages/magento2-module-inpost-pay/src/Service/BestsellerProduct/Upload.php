3d62482cb52b5847715f98888d6157882b612a0e
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2024-10-14 15:40:14 +0200

    INPAY-245 - Added adminhtml controllers for upload and download. Updated cps_whitelist.xml for inpost.

---
+++ packages/magento2-module-inpost-pay/src/Service/BestsellerProduct/Upload.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InPost\InPostPay\Service\BestsellerProduct;
+
+class Upload
+{
+    /**
+     * @return void
+     */
+    public function execute(): void
+    {
+        //TODO::implement uploading products
+    }
+}
