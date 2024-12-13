1db835d5c29448b8b63e1213c8ca5c6e5e7e9809
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2024-12-13 08:45:23 +0100

    INPAY-310 - fixed validation problem

--- packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
+++ packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
@@ -226,18 +226,19 @@ define([
             }
 
             if (!productId) {
-                return Promise.reject('Product id not found');
+                return Promise.reject(new Error('Product id not found'));
             }
 
             var $productInput = $('[name="product"][value="' + productId + '"]');
             var $productForm = $productInput.parent('#product_addtocart_form');
 
             if (!$productForm.length) {
-                return Promise.reject('Problem with product configuration');
+                return Promise.reject(new Error('UNDELIVERABLE_PRODUCT'))
+
             }
 
             if (!$productForm.validation('isValid')) {
-                return Promise.reject('Product is invalid');
+                return Promise.reject(new Error('UNDELIVERABLE_PRODUCT'))
             }
 
             var isProductAdded = this.checkIfProductIsAdded(productId, customerData.get("cart")(), $productForm)
