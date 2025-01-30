4b1cc4a2f96729ee3380176615e08e5cc9af5a3c
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2025-01-30 10:45:02 +0100

    INPAY-328 - Work in progress commit.

--- packages/magento2-module-inpost-pay/src/Observer/Order/Email/Sender/InPostPayOrderEmailSenderObserver.php
+++ packages/magento2-module-inpost-pay/src/Observer/Order/Email/Sender/InPostPayOrderEmailSenderObserver.php
@@ -7,19 +7,27 @@ namespace InPost\InPostPay\Observer\Order\Email\Sender;
 use InPost\InPostPay\Api\Data\InPostPayOrderInterface;
 use InPost\InPostPay\Api\InPostPayOrderRepositoryInterface;
 use InPost\InPostPay\Registry\Order\Email\Sender\InPostPayOrderEmailSenderRegistry;
+use Magento\Framework\Api\SearchCriteriaBuilderFactory;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\DataObject;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\Order;
 
 class InPostPayOrderEmailSenderObserver implements ObserverInterface
 {
     /**
+     * @param OrderRepositoryInterface $orderRepository
+     * @param SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory
      * @param InPostPayOrderRepositoryInterface $inPostPayOrderRepository
      * @param InPostPayOrderEmailSenderRegistry $inPostPayOrderEmailSenderRegistry
      */
     public function __construct(
+        private readonly OrderRepositoryInterface $orderRepository,
+        private readonly SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory,
         private readonly InPostPayOrderRepositoryInterface $inPostPayOrderRepository,
         private readonly InPostPayOrderEmailSenderRegistry $inPostPayOrderEmailSenderRegistry
     ) {
@@ -55,11 +63,34 @@ class InPostPayOrderEmailSenderObserver implements ObserverInterface
     private function getInPostPayOrder(Order $order): ?InPostPayOrderInterface
     {
         try {
-            $orderId = is_scalar($order->getId()) ? (int)$order->getId() : 0;
+            $orderId = is_scalar($order->getId()) ? (int)$order->getId() : null;
 
-            return $this->inPostPayOrderRepository->getByOrderId($orderId);
+            if ($orderId) {
+                return $this->inPostPayOrderRepository->getByOrderId($orderId);
+            } else {
+                $orderId = $this->getOrderIdByIncrementId((string)$order->getIncrementId());
+            }
+
+            return $this->inPostPayOrderRepository->getByOrderId((int)$orderId);
         } catch (NoSuchEntityException $e) {
             return null;
         }
+    }
+
+    /**
+     * @param string $incrementId
+     * @return Order|null
+     */
+    private function getOrderIdByIncrementId(string $incrementId): ?int
+    {
+        /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
+        $searchCriteriaBuilder = $this->searchCriteriaBuilderFactory->create();
+        $criteria = $searchCriteriaBuilder
+            ->addFilter(OrderInterface::INCREMENT_ID, $incrementId)
+            ->create();
+        $orders = $this->orderRepository->getList($criteria)->getItems();
+        $order = count($orders) ? $orders[0] : null;
+
+        return ($order && is_scalar($order->getId())) ? (int)$order->getId() : null;
     }
 }
