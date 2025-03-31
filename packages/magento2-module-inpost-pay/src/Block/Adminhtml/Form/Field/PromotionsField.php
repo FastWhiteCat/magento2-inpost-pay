3860636be70e16018556b164468e4587a6d4a8e7
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2025-03-31 14:47:39 +0200

    INPAY-233 - Added validation and limit to promotions config.

--- packages/magento2-module-inpost-pay/src/Block/Adminhtml/Form/Field/PromotionsField.php
+++ packages/magento2-module-inpost-pay/src/Block/Adminhtml/Form/Field/PromotionsField.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace InPost\InPostPay\Block\Adminhtml\Form\Field;
 
 use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\DataObject;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\View\Element\BlockInterface;
@@ -12,9 +13,37 @@ class PromotionsField extends AbstractFieldArray
 {
     public const MAGENTO_CART_RULE_ID_FIELD = 'magento_cart_rule_id';
     public const PROMOTION_URL_FIELD = 'promotion_url';
+    public const PROMOTIONS_CONFIG_FIELD_ID = 'payment_other_inpost_pay_inpost_pay_promotions_promotions_mapping';
+    public const MAX_PROMOTIONS_LIMIT = 5;
 
-    /** @var BlockInterface|null */
-    private $cartRuleRenderer;
+    private ?BlockInterface $cartRuleRenderer = null;
+
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element): string
+    {
+        // Call the default rendering logic
+        $html = parent::_getElementHtml($element);
+        $html = '<div id="' . self::PROMOTIONS_CONFIG_FIELD_ID . '">' . $html . '</div>';
+
+        $enabledAddButtonText = __('Add another promotion')->render();
+        $disabledAddButtonText = __('Promotions limit reached. Please remove one before adding another')->render();
+
+        $html .= '<script>
+            require(["InPost_InPostPay/js/system/config/manage-promotions-limit"], function(limitRows){
+                limitRows({
+                    inputId: "' . self::PROMOTIONS_CONFIG_FIELD_ID . '",
+                    maxRowLimit: ' . self::MAX_PROMOTIONS_LIMIT . ',
+                    enabledAddButtonText: "' . $enabledAddButtonText . '",
+                    disabledAddButtonText: "' . $disabledAddButtonText . '",
+                });
+            });
+        </script>';
+
+        return $html;
+    }
 
     /**
      * @inheritDoc
@@ -29,9 +58,11 @@ class PromotionsField extends AbstractFieldArray
 
         $this->addColumn(self::PROMOTION_URL_FIELD, [
             'label' => __('Promotion description url'),
+            'class' => 'required-entry validate-url',
         ]);
 
         $this->_addAfter = false;
+        $this->_addButtonLabel = __('Add another promotion')->render();
     }
 
     /**
