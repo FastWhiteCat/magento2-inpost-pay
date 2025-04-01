bfb4e6ed9a0bd5f8446d708bd14bd8e5970b4800
Maciej Posacki <maciej.posacki@fastwhitecat.com>
2025-04-01 15:17:45 +0200

    INPAY-342 - rollback apiurl param

--- packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
+++ packages/magento2-module-inpost-pay/src/view/frontend/web/js/inpostPayMerchantWidget.js
@@ -101,6 +101,7 @@ define([
              * @property {string} language
              * @property {unboundWidgetClicked} unboundWidgetClicked
              * @property {handleBasketEvent} handleBasketEvent
+             * @property {string} apiBaseUrl
              * @property {boolean} webView
              */
             var widgetOptions = $.extend({
@@ -110,6 +111,7 @@ define([
                 handleBasketEvent: this.handleBasketEvent.bind(this),
             }, {
                 language: config.language ? config.language : undefined,
+                apiBaseUrl: config.apiBaseUrl ? config.apiBaseUrl : undefined,
                 webView: config.webView ? config.webView : undefined,
             });
 
