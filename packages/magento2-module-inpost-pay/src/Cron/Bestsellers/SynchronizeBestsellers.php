bf4b7ec5f4aa98046f81d0500f87dc580f79caf9
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2025-05-27 10:28:12 +0200

    INPAY-439 - Fixed bestseller products synchronization

--- packages/magento2-module-inpost-pay/src/Cron/Bestsellers/SynchronizeBestsellers.php
+++ packages/magento2-module-inpost-pay/src/Cron/Bestsellers/SynchronizeBestsellers.php
@@ -3,9 +3,7 @@ declare(strict_types=1);
 
 namespace InPost\InPostPay\Cron\Bestsellers;
 
-use InPost\InPostPay\Model\Config\Source\BestsellersSynchronizeMode;
 use InPost\InPostPay\Provider\Config\BestsellersCronConfigProvider;
-use InPost\InPostPay\Service\BestsellerProduct\Download;
 use InPost\InPostPay\Service\BestsellerProduct\Upload;
 use Magento\Framework\Exception\LocalizedException;
 use Psr\Log\LoggerInterface;
@@ -14,7 +12,6 @@ class SynchronizeBestsellers
 {
     public function __construct(
         private readonly BestsellersCronConfigProvider $bestsellersCronConfigProvider,
-        private readonly Download $download,
         private readonly Upload $upload,
         private readonly LoggerInterface $logger
     ) {
@@ -26,21 +23,12 @@ class SynchronizeBestsellers
     public function execute(): void
     {
         if ($this->bestsellersCronConfigProvider->isCronEnabled()) {
-            $mode = $this->bestsellersCronConfigProvider->getSynchronizationMode();
-
             try {
-                if ($mode === BestsellersSynchronizeMode::DOWNLOAD) {
-                    $this->download->execute();
-                } elseif ($mode === BestsellersSynchronizeMode::UPLOAD) {
-                    $this->upload->execute();
-                }
-
-                $this->logger->error(
-                    sprintf('Bestsellers Synchronization CRON Job [mode:%s] success!', $mode)
-                );
+                $this->upload->execute();
+                $this->logger->debug('Bestsellers Synchronization CRON Job success!');
             } catch (LocalizedException $e) {
                 $this->logger->error(
-                    sprintf('Bestsellers Synchronization CRON Job [mode:%s] error: %s', $mode, $e->getMessage())
+                    sprintf('Bestsellers Synchronization CRON Job error: %s', $e->getMessage())
                 );
             }
         }
