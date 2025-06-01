f348f1e7f6999edad30e3642f4c5d07e745d98e8
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2025-06-01 17:25:13 +0200

    INPAY-441 - cron product synchro fix.

--- packages/magento2-module-inpost-pay/src/Service/DataTransfer/MagentoBestsellerToInPostPayBestseller/BestsellerPriceDataTransfer.php
+++ packages/magento2-module-inpost-pay/src/Service/DataTransfer/MagentoBestsellerToInPostPayBestseller/BestsellerPriceDataTransfer.php
@@ -11,20 +11,24 @@ use InPost\InPostPay\Service\Calculator\DecimalCalculator;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Pricing\Price\FinalPrice;
+use Magento\Framework\App\Area;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Store\Model\Website;
+use Magento\Store\Model\App\Emulation as StoreEmulator;
 
 class BestsellerPriceDataTransfer implements MagentoBestsellerToInPostPayBestsellerDataTransferInterface
 {
     /**
      * @param ProductRepositoryInterface $productRepository
      * @param StoreManagerInterface $storeManager
+     * @param StoreEmulator $storeEmulator
      */
     public function __construct(
         private readonly ProductRepositoryInterface $productRepository,
-        private readonly StoreManagerInterface $storeManager
+        private readonly StoreManagerInterface $storeManager,
+        private readonly StoreEmulator $storeEmulator
     ) {
     }
 
@@ -39,9 +43,10 @@ class BestsellerPriceDataTransfer implements MagentoBestsellerToInPostPayBestsel
         BestsellerProductInterface $bestsellerProduct
     ): void {
         $storeId = $this->getDefaultStoreIdForWebsiteId($magentoBestsellerProduct->getWebsiteId());
+        $this->storeEmulator->startEnvironmentEmulation($storeId, Area::AREA_FRONTEND, true);
 
         /** @var Product $product */
-        $product = $this->productRepository->get($magentoBestsellerProduct->getSku(), false, $storeId);
+        $product = $this->productRepository->get($magentoBestsellerProduct->getSku(), false, $storeId, true);
         $price = $bestsellerProduct->getPrice();
 
         $finalPrice = $product->getPriceInfo()->getPrice(FinalPrice::PRICE_CODE)->getAmount();
@@ -55,6 +60,7 @@ class BestsellerPriceDataTransfer implements MagentoBestsellerToInPostPayBestsel
 
         $bestsellerProduct->setPrice($price);
         $bestsellerProduct->setCurrency($this->getWebsiteCurrencyCode($magentoBestsellerProduct->getWebsiteId()));
+        $this->storeEmulator->stopEnvironmentEmulation();
     }
 
     /**
