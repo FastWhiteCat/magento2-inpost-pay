28b1a01be9b6757bb8f196492e775c5f27315e93
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2025-08-05 23:15:20 +0200

    INPAY-122 - Upgraded InPost_InPostPay Module to newest 2.2.2 version.

--- packages/magento2-module-inpost-pay/src/Enum/InPostBestsellerProductStatus.php
+++ packages/magento2-module-inpost-pay/src/Enum/InPostBestsellerProductStatus.php
@@ -10,6 +10,10 @@ namespace InPost\InPostPay\Enum;
  */
 enum InPostBestsellerProductStatus: string
 {
+    public const ACTIVE_VALUE = 'ACTIVE';
+    public const INACTIVE_VALUE = 'INACTIVE';
+    public const UNKNOWN_VALUE = 'UNKNOWN';
+
     case ACTIVE = 'ACTIVE';
     case INACTIVE = 'INACTIVE';
     case UNKNOWN = 'UNKNOWN';
