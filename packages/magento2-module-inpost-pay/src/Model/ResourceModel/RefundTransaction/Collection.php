9eef05c46a25be8df82d8632c897115f26a4fcd2
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2026-01-20 11:47:25 +0100

    INPAY-577 - injected loggers and translations

--- packages/magento2-module-inpost-pay/src/Model/ResourceModel/RefundTransaction/Collection.php
+++ packages/magento2-module-inpost-pay/src/Model/ResourceModel/RefundTransaction/Collection.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace InPost\InPostPay\Model\ResourceModel\RefundTransaction;
 
+use InPost\InPostPay\Api\Data\RefundTransactionInterface;
 use InPost\InPostPay\Model\RefundTransaction;
 use InPost\InPostPay\Model\ResourceModel\RefundTransaction as RefundTransactionResource;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection
 {
+    protected $_eventPrefix = RefundTransactionInterface::ENTITY_NAME;
+    protected $_eventObject = RefundTransactionInterface::ENTITY_NAME;
+    protected $_idFieldName = RefundTransactionInterface::MAPPING_ID;
+
     protected function _construct(): void
     {
         $this->_init(RefundTransaction::class, RefundTransactionResource::class);
