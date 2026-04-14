e47e59c0919991f7570b30d9b912db5ffb4a0eaf
Lukasz Bajsarowicz <lukasz.bajsarowicz@fastwhitecat.com>
2026-04-14 11:25:13 +0200

    INPAY-600 Update module version fetching (use Composer's method)

--- packages/magento2-module-inpost-pay/src/Block/Adminhtml/System/Config/Form/ModuleVersion.php
+++ packages/magento2-module-inpost-pay/src/Block/Adminhtml/System/Config/Form/ModuleVersion.php
@@ -1,38 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace InPost\InPostPay\Block\Adminhtml\System\Config\Form;
 
+use Composer\InstalledVersions;
 use Exception;
-use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field;
-use Magento\Framework\Component\ComponentRegistrar;
-use Magento\Framework\Component\ComponentRegistrarInterface;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Framework\Filesystem\Directory\ReadFactory;
 
 class ModuleVersion extends Field
 {
     public const UNKNOWN_VERSION = 'Unknown';
 
     /**
-     * @param Context $context
-     * @param ComponentRegistrarInterface $componentRegistrar
-     * @param ReadFactory $readFactory
-     * @param array $data
-     */
-    public function __construct(
-        private readonly ComponentRegistrarInterface $componentRegistrar,
-        private readonly ReadFactory $readFactory,
-        Context $context,
-        array $data = []
-    ) {
-        parent::__construct($context, $data);
-    }
-
-    /**
      * Return element html
      *
-     * @param  AbstractElement $element
+     * @param AbstractElement $element
      * @return string
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
@@ -48,7 +32,7 @@ class ModuleVersion extends Field
      */
     public function getVersion(): string
     {
-        return $this->getComposerVersion((string)$this->getModuleName());
+        return $this->getComposerVersion('inpost/magento2-module-inpost-pay');
     }
 
     /**
@@ -58,24 +42,11 @@ class ModuleVersion extends Field
     public function getComposerVersion(string $moduleName): string
     {
         try {
-            $path = $this->componentRegistrar->getPath(
-                ComponentRegistrar::MODULE,
-                $moduleName
-            );
-            // @phpstan-ignore-next-line
-            $directoryRead = $this->readFactory->create((string)$path);
-            $composerJsonData = $directoryRead->readFile('composer.json');
-
-            if ($composerJsonData) {
-                $data = json_decode($composerJsonData);
-
-                // @phpstan-ignore-next-line
-                return !empty($data->version) ? $data->version : __('Unknown')->render();
-            }
+            return InstalledVersions::getVersion($moduleName);
         } catch (Exception $e) {
-            return __('Unknown')->render();
+            // phpcs:ignore Magento2.CodeAnalysis.EmptyBlock
         }
 
-        return __('Unknown')->render();
+        return __(self::UNKNOWN_VERSION)->render();
     }
 }
